da538919160709f593ff28944774afa486554435
Matt <md3es@virginia.edu>
2016-12-23 14:28:30 -0500

    initial input working

---
+++ play.py
@@ -0,0 +1,12 @@
+from game import move
+import sys
+
+print('Which Player are you?')
+sys.stdout.flush()
+p = int(raw_input()) - 1
+
+print('Which hole would you like to move?')
+sys.stdout.flush()
+h = int(raw_input())
+
+move(p,h)
