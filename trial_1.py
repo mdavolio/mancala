312942859c034a6e7bfd44730341d40d1da747a6
Matt <md3es@virginia.edu>
2016-12-23 13:42:44 -0500

    scoring fixed

--- trial_1.py
+++ trial_1.py
@@ -1,10 +1,14 @@
 import numpy as np
 
-grid  = np.array([[0,1,2,3,4,5], [4,4,4,4,4,4]])
+grid  = np.array([[4,4,4,4,4,4], [4,4,4,4,4,4]])
 print(grid)
 
+p1_score = 0
+p2_score = 0
 
 def score():
+    global p1_score
+    global p2_score
     if player == 1:
         p1_score += 1
         return p1_score
@@ -15,12 +19,16 @@ def score():
     return count
 
 def move(x,y):
+    global player
+    global count
+
     if x == 0:
         player = 1
     elif x == 1:
         player = 2
 
     count = grid[x,y]
+
     grid[x,y] = 0
 
     while count > 0:
@@ -45,6 +53,8 @@ def move(x,y):
         count -= 1
         print(count)
         print(grid)
+        print(p1_score)
+        print(p2_score)
     return
-    
-move(0,3)
+
+move(0,0)
