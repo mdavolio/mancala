b5c44c30db6a64cb30692f31a5f369c1034cf8f6
Matt <md3es@virginia.edu>
2016-12-23 14:39:32 -0500

    stone check didn't actually work

--- play.py
+++ play.py
@@ -1,16 +1,15 @@
 from game import *
 import sys
 
-while p1_stones > 0 and p2_stones > 0:
-    print('Which Player are you?')
-    sys.stdout.flush()
-    p = int(raw_input()) - 1
 
-    print('Which hole would you like to move?')
-    sys.stdout.flush()
-    h = int(raw_input())
 
-    move(p,h)
 
-    print(p1_score)
-    print(p2_score)
+print('Which Player are you?')
+sys.stdout.flush()
+p = int(raw_input()) - 1
+
+print('Which hole would you like to move?')
+sys.stdout.flush()
+h = int(raw_input())
+
+move(p,h)
