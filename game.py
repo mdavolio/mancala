3025966b553f2913034da852d2a56310fc8b39d1
Matt <md3es@virginia.edu>
2016-12-23 14:47:03 -0500

    continual game now actually works

--- game.py
+++ game.py
@@ -5,6 +5,8 @@ print(grid)
 
 p1_score = 0
 p2_score = 0
+stones_1 = np.sum(grid[0,:])
+stones_2 = np.sum(grid[1,:])
 
 def score():
     global p1_score
@@ -51,8 +53,12 @@ def move(x,y):
                 grid[x,y] += 1
 
         count -= 1
-        print(count)
+
+        stones_1 = np.sum(grid[0,:])
+        stones_2 = np.sum(grid[1,:])
+
+        # print(count)
         print(grid)
-        #print(p1_score)
-        #print(p2_score)
+        # print(p1_score)
+        # print(p2_score)
     return
--- play.py
+++ play.py
@@ -2,14 +2,13 @@ from game import *
 import sys
 
 
+while stones_1 > 0 and stones_2 > 0:
+    print('Which Player are you?')
+    sys.stdout.flush()
+    p = int(raw_input()) - 1
 
+    print('Which hole would you like to move?')
+    sys.stdout.flush()
+    h = int(raw_input())
 
-print('Which Player are you?')
-sys.stdout.flush()
-p = int(raw_input()) - 1
-
-print('Which hole would you like to move?')
-sys.stdout.flush()
-h = int(raw_input())
-
-move(p,h)
+    move(p,h)
