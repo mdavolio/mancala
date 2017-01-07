2a0a7338471c922c12d1295e97c0c94e1ac45dfc
Erik <erik.langenborg@gmail.com>
2017-01-06 20:02:53 -0500

    Basic tests for making a move

--- test.py
+++ test.py
@@ -17,6 +17,26 @@ class TestInit(unittest.TestCase):
         self.assertEqual(Game().board_render(),
                          """     3  1  0  0  0  0\n  0                    0 \n     4  4  4  4  4  4""")
 
+    def test_board_raw(self):
+        self.assertEqual(Game()._board,
+                         [3, 1, 0, 0, 0, 0, 0, 4, 4, 4, 4, 4, 4, 0])
+
+
+class TestMoves(unittest.TestCase):
+
+    def test_move_00(self):
+        g = Game()
+        g.move(0)
+        self.assertEqual(g._board,
+                         [0, 2, 1, 1, 0, 0, 0, 4, 4, 4, 4, 4, 4, 0])
+        self.assertEqual(g.score(), (0, 0))
+
+    def test_move_01(self):
+        g = Game()
+        g.move(1)
+        self.assertEqual(g._board,
+                         [3, 0, 1, 0, 0, 0, 0, 4, 4, 4, 4, 4, 4, 0])
+
 
 if __name__ == '__main__':
     unittest.main()
