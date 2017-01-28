3f47b3f371793b18957383679557f1804488e380
Matt <md3es@virginia.edu>
2017-01-27 20:51:35 -0500

    fix test 5

--- test.py
+++ test.py
@@ -52,7 +52,7 @@ class TestMoves(unittest.TestCase):
         g.move(0)
         g.move(11)
         self.assertEqual(g._board,
-                         [1, 5, 5, 5, 5, 4, 0, 4, 4, 4, 0, 5, 5, 1])
+                         [1, 6, 5, 5, 5, 4, 0, 4, 4, 4, 4, 0, 5, 1])
         self.assertEqual(g.score(), (0, 1))
         self.assertEqual(g.turn_player(), 1)
 
