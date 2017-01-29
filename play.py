7b7227544a6aaa0321aaf1f845d821de596c74ca
Matt <md3es@virginia.edu>
2017-01-29 16:36:38 -0500

    functionize play

--- play.py
+++ play.py
@@ -4,24 +4,25 @@
 from game import Game
 import sys
 
-game_current = Game()
+def Play():
+    game_current = Game()
 
-while not game_current.over():
-    # print("\n" * 100) # Screen clearing *hack*
-    print("Play Mancala!")
-    print("")
-    print("Score | Player One | Player Two")
-    print("        {0: >10} | {1: >10}".format(
-          game_current.score()[0], game_current.score()[1]))
-    print(game_current.board_render())
-    print("Player {0}'s Turn".format(game_current.turn_player()))
-    i = input()
-    if (i == 'q'):
-        break
-#    try:
-    m = int(i)
-    game_current.move(m)
-'''    except:
-        print("Unable to play that move...")
-        pass
-'''
+    while not game_current.over():
+        # print("\n" * 100) # Screen clearing *hack*
+        print("Play Mancala!")
+        print("")
+        print("Score | Player One | Player Two")
+        print("        {0: >10} | {1: >10}".format(
+              game_current.score()[0], game_current.score()[1]))
+        print(game_current.board_render())
+        print("Player {0}'s Turn".format(game_current.turn_player()))
+        i = input()
+        if (i == 'q'):
+            break
+    #    try:
+        m = int(i)
+        game_current.move(m)
+    '''    except:
+            print("Unable to play that move...")
+            pass
+    '''
