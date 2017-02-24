ea4a8e5eb242f8a9e6d5751775ac17b5840529f0
Erik <erik.langenborg@gmail.com>
2017-02-24 02:07:56 -0500

    Updated to note the current turn

--- www/index.js
+++ www/index.js
@@ -1,6 +1,7 @@
 var board_current = [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0];
 var player_turn = 1;
 
+
 const cell_click = move => {
   console.log(`Click ${move}`);
   $.ajax({
@@ -13,10 +14,16 @@ const cell_click = move => {
       board_current = e.board;
       player_turn = e.player_turn;
       console.log(e);
+      render_player(player_turn);
       render_board(board_current);
     }
   });
 };
+const render_player = (turn) => {
+  const turn_elm = $('#player_turn');
+  turn_elm.html(`Player ${player_turn == 1 ? 'One' : 'Two'}'s Turn`);
+  turn_elm.css('color', player_turn == 1 ? 'rgba(33, 174, 255, 0.93)' : 'rgba(255, 0, 0, 0.93)');
+}
 
 const i_to_str = i => i >= 10 ? `${i}` : `0${i}`;
 const board_to_str = R.pipe(
@@ -30,7 +37,8 @@ const over_cells = (cb) => {
   )(14);
 };
 const cell_from_id = i => $(`#cell-${i_to_str(i)}`);
-const render_board = (board) => over_cells(i => cell_from_id(i).html(board[i]));
+const render_board = (board) => over_cells(i => cell_from_id(i).html(board[i] > 0 ? board[i] : ''));
 over_cells(i => cell_from_id(i).on('touchstart click', evt => cell_click(i)));
 
 render_board(board_current);
+render_player(player_turn);
