32262a3eacc598f0713d9e1ae7faad50597e4649
Erik <erik.langenborg@gmail.com>
2017-03-02 00:43:54 -0500

    Added an abstract agent class - extremely minimal

---
+++ mancala/agent.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+Abstract Agent for the a Mancala AI
+"""
+
+
+class Agent():
+    """Abstract Class for agent to play Mancala."""
+
+    def move(self, game=None):
+        """Return a move index based on a game"""
+        self._move(game)
+
+    def _move(self, game):
+        """Return a move index based on a game"""
+        raise NotImplementedError("Class {} doesn't implement aMethod()".format(
+            self.__class__.__name__))
