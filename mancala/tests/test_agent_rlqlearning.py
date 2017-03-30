bed98d7d3acdc7387cd2e6dd68267c013440814e
Erik <erik.langenborg@gmail.com>
2017-03-30 18:02:18 -0400

    Weighted picking

--- mancala/tests/test_agent_rlqlearning.py
+++ mancala/tests/test_agent_rlqlearning.py
@@ -12,14 +12,40 @@ from mancala.game import Game
 class TestAgentRLQLearning(unittest.TestCase):
     """Tests for the RL Q-Learning Agent class"""
 
-
     def test_learning_small(self):
         """Do some small learning with Random opponent"""
         agent = AgentQBinary()
         values = agent.learn_policy(2)
-        self.assertEqual(len(values), 37)
+        self.assertEqual(len(values), 41)
         for action_values in values.values():
             self.assertEqual(len(action_values), 6)
+
+    def test_weighted_pick(self):
+        """Make weighted picks"""
+        values = [1, 2, 5]
+        pick = AgentQBinary.weighted_pick(values, 1)
+        self.assertEqual(pick, 1)
+        pick = AgentQBinary.weighted_pick(values, 2)
+        self.assertEqual(pick, 2)
+        pick = AgentQBinary.weighted_pick(values, 3)
+        self.assertEqual(pick, 2)
+        pick = AgentQBinary.weighted_pick(values, 5)
+        self.assertEqual(pick, 2)
+        pick = AgentQBinary.weighted_pick(values, 6)
+        self.assertEqual(pick, 2)
+
+    def test_max_pick(self):
+        """Make pick of the max values"""
+        values = [1, 2, 5]
+        for idx in range(0, 20):
+            pick = AgentQBinary.max_pick(values, idx)
+            self.assertEqual(pick, 2)
+
+        values = [1, 2, 2, 1]
+        for idx in range(0, 20):
+            pick = AgentQBinary.max_pick(values, idx)
+            result = pick == 1 or pick == 2
+            self.assertTrue(result)
 
 
 if __name__ == '__main__':
