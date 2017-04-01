8a8b741057cdcacb98780c7fa0871b6a229363e6
Erik <erik.langenborg@gmail.com>
2017-03-31 23:34:40 -0400

    Generating arena results

--- mancala/arena.py
+++ mancala/arena.py
@@ -4,6 +4,7 @@ Mancala Arena object
 """
 
 import itertools
+from mancala.game import Game
 
 
 class Arena():
@@ -27,9 +28,32 @@ class Arena():
         self._names = list(sorted(map(lambda t: t[0], agents)))
 
         # this ensures all agent pairs and pairs with themselves
-        combos = list(itertools.combinations(agents, 2)) + list(zip(agents, agents))
+        combos = list(itertools.combinations(agents, 2)) + \
+            list(zip(agents, agents))
         self._combos = sorted(combos, key=lambda t: t[0][0] + '_' + t[1][0])
+        self._results = list(map(lambda t: Arena._handle_combo(
+            t[1], t[0], games_to_play), enumerate(self._combos)))
+
+    @staticmethod
+    def _handle_combo(combo, seed, games_to_play):
+        wins = 0
+        for idx in range(0, games_to_play):
+            game = Game()
+            agent_one = (combo[0][1])(seed + idx)
+            agent_two = (combo[1][1])(seed + idx)
+            while not game.over():
+                if game.turn_player() == 1:
+                    game.move(agent_one.move(game))
+                else:
+                    game.move(agent_two.move(game))
+            if game.score()[0] > game.score()[1]:
+                wins = wins + 1
+        return (combo[0][0], combo[1][0], wins)
 
     def names(self):
         """Sorted names of agents"""
         return self._names[:]
+
+    def results(self):
+        """Sorted results of agents"""
+        return self._results[:]
--- mancala/tests/test_arena.py
+++ mancala/tests/test_arena.py
@@ -26,7 +26,39 @@ class TestArenaNames(unittest.TestCase):
             ("Random C", lambda seed: AgentRandom(seed)),
             ("Random B", lambda seed: AgentRandom(seed))
         ], 5)
-        self.assertListEqual(arena.names(), ["Random A", "Random B", "Random C"])
+        self.assertListEqual(
+            arena.names(), ["Random A", "Random B", "Random C"])
+
+
+class TestArenaResults(unittest.TestCase):
+    """Test the arena"""
+
+    def test_init_single(self):
+        """Define the arena with one version"""
+        arena = Arena([
+            ("Random", lambda seed: AgentRandom(seed))
+        ], 5)
+        results = arena.results()
+        self.assertEqual(len(results), 1)
+        self.assertListEqual(results, [("Random", "Random", 3)])
+
+    def test_init_multiple(self):
+        """Define the arena with several agents"""
+        arena = Arena([
+            ("Random A", lambda seed: AgentRandom(seed)),
+            ("Random C", lambda seed: AgentRandom(seed)),
+            ("Random B", lambda seed: AgentRandom(seed))
+        ], 5)
+        results = arena.results()
+        self.assertEqual(len(results), 6)
+        self.assertListEqual(results, [
+            ("Random A", "Random A", 3),
+            ("Random A", "Random B", 3),
+            ("Random A", "Random C", 2),
+            ("Random B", "Random B", 1),
+            ("Random C", "Random B", 1),
+            ("Random C", "Random C", 1),
+        ])
 
 
 if __name__ == '__main__':
