3cc3cd95584c33a67cce2d342f451923d699fb0d
Matthew Davolio <md3es@virginia.edu>
2017-05-07 12:19:03 -0400

    updated results generating code

--- analyze_heatmap.py
+++ analyze_heatmap.py
@@ -8,9 +8,12 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("arena.results.csv", index_col=0)
 
-f, ax = plt.subplots(figsize=(6, 4))
+f, ax = plt.subplots(figsize=(8, 4))
 plt.title("Performance Results", fontsize=18)
 ax = sns.heatmap((df*100).astype(int), annot=True, fmt="d", cmap='BuGn', linewidth=0.30)
+ax.set_xlabel('Player 1')
+ax.set_ylabel('Player 2')
+plt.yticks(rotation=0) 
 # ax = sns.heatmap((df*100).astype(int), annot=True, fmt="d", cmap='RdBu', linewidth=0.30)
 
 fig = ax.get_figure()
